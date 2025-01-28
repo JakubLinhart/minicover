d7576a9649826eff1b4a39d4307a823bc119ba2d
Jakub Linhart <jakub.linhart@gmail.com>
2025-01-28 18:59:21 +0100

    file lock issue mitigation

--- src/MiniCover.Core/Hits/HitsReader.cs
+++ src/MiniCover.Core/Hits/HitsReader.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
+using System.Threading;
 using MiniCover.HitServices;
 
 namespace MiniCover.Core.Hits
@@ -22,7 +24,7 @@ namespace MiniCover.Core.Hits
             {
                 foreach (var hitFile in _fileSystem.Directory.GetFiles(path, "*.hits"))
                 {
-                    using (var fileStream = _fileSystem.File.Open(hitFile, FileMode.Open, FileAccess.Read))
+                    using (var fileStream = OpenWithRetry(hitFile, 5, 100))
                     {
                         contexts.AddRange(HitContext.Deserialize(fileStream));
                     }
@@ -30,6 +32,29 @@ namespace MiniCover.Core.Hits
             }
 
             return new HitsInfo(contexts);
+        }
+
+        private Stream OpenWithRetry(string fileName, int maxRetries, int backoffDelayMs)
+        {
+            var retryCount = 0;
+            while (true)
+            {
+                try
+                {
+                    return _fileSystem.File.Open(fileName, FileMode.Open, FileAccess.Read);
+                }
+                catch (IOException)
+                {
+                    retryCount++;
+                    if (retryCount > maxRetries)
+                    {
+                        throw; // Re-throw the exception if the maximum number of retries is reached
+                    }
+                    // Exponential backoff: 2^retryCount * 100 milliseconds
+                    var delay = (int)Math.Pow(2, retryCount) * backoffDelayMs;
+                    Thread.Sleep(delay);
+                }
+            }
         }
     }
 }
--- src/MiniCover.HitServices/MethodScope.cs
+++ src/MiniCover.HitServices/MethodScope.cs
@@ -63,7 +63,7 @@ namespace MiniCover.HitServices
 
                 var fileName = Path.Combine(_hitsPath, $"{_hitContext.Id}.hits");
 
-                using (var fileStream = File.Open(fileName, FileMode.Create))
+                using (var fileStream = File.Open(fileName, FileMode.Create, FileAccess.Write, FileShare.Read))
                 {
                     _hitContext.Serialize(fileStream);
                     fileStream.Flush();
